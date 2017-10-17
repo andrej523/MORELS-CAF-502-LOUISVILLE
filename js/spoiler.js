dd2e4cd4c5da8fef5d762d5a5b4c0e7a6a675fb0
Andrej <acucilovic23@gmail.com>
2017-10-16 21:03:00 -0400

    Added navigation bar with custom stylings and scroll.js to handle scroll event.

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,10 +1,10 @@
 $('.spoiler span').hide(); // hide the element (<p> in this case) with the class 'spoiler' that contains a child <span> element
 
-$('.spoiler').append('<button>Reveal Spoiler!</button>'); // add a <button> element to the element (<p> in this case) with the class 'spoiler'
+$('.spoiler').append("<button class='spoiler-btn'>Reveal Spoiler!</button>"); // add a <button> element to the element (<p> in this case) with the class 'spoiler-btn'
 
 $('.spoiler button').click(function () // when the user clicks on the previously created <button> element...
                                     {
-                                        $(this).prev().show(); // ...show the previous element (<span> element with the class 'spoiler' in this case) of the selected specific object (previously created <button> element in this case)
+                                        $(this).prev().show(); // ...show the previous element (<span> element with the class 'spoiler-btn' in this case) of the selected specific object (previously created <button> element in this case)
     
                                         $(this).remove(); // remove the selected specific object (previously created <button> element in this case) from the page
                                     }
