66c650ea29fc3c59a502e69f0160df8236a873c1
Andrej <acucilovic23@gmail.com>
2017-11-01 21:37:07 -0400

    Added flexslider to display slideshow of user testimonials and modified stylings for slides and their transition animation.

--- js/scroll.js
+++ js/scroll.js
@@ -1,23 +1,21 @@
-(function($)
-         { 
-             // activate scrollspy to add active class to navbar items on scroll
-             $('body').scrollspy( // Bootstrap plugin to automatically update nav target based on scroll position
-                                {
-                                    target : '#mainNav', offset : 54 // update nav target when offset is 54px
-                                }
-                                );
+(function ($)
+{
+    // activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy( // Bootstrap plugin to automatically update nav target based on scroll position
+    {
+        target: '#mainNav',
+        offset: 54 // update nav target when offset is 54px
+    });
 
-             // collapse the navbar when page is scrolled
-             $(window).scroll(function() // jQuery scroll event
-                                      {
-                                          if ($('#mainNav').offset().top > 100) // conditional statement to check if top offset is greater than 100px...
-                                          {
-                                              $('#mainNav').addClass('navbar-shrink'); // ...add 'navbar-shrink' class to nav target
-                                          } else // conditional statement to check if top offset is anything other than greater than 100px...
-                                            {
-                                                $('#mainNav').removeClass('navbar-shrink'); // ...remove 'navbar-shrink' class from nav target
-                                            }
-                                      }
-                             );
-         }  
-)(jQuery); // call this function under jQuery namespace
+    // collapse the navbar when page is scrolled
+    $(window).scroll(function () // jQuery scroll event
+    {
+        if ($('#mainNav').offset().top > 100) // conditional statement to check if top offset is greater than 100px...
+        {
+            $('#mainNav').addClass('navbar-shrink'); // ...add 'navbar-shrink' class to nav target
+        } else // conditional statement to check if top offset is anything other than greater than 100px...
+          {
+              $('#mainNav').removeClass('navbar-shrink'); // ...remove 'navbar-shrink' class from nav target
+          }
+    });
+})(jQuery); // call this function under jQuery namespace
--- js/spoiler.js
+++ js/spoiler.js
@@ -3,9 +3,7 @@ $('.spoiler span').hide(); // hide the element (<p> in this case) with the class
 $('.spoiler').append("<button class='spoiler-btn'>Reveal Spoiler!</button>"); // add a <button> element to the element (<p> in this case) with the class 'spoiler-btn'
 
 $('.spoiler button').click(function () // when the user clicks on the previously created <button> element...
-                                    {
-                                        $(this).prev().show(); // ...show the previous element (<span> element with the class 'spoiler-btn' in this case) of the selected specific object (previously created <button> element in this case)
-    
-                                        $(this).remove(); // remove the selected specific object (previously created <button> element in this case) from the page
-                                    }
-                          );
+{
+    $(this).prev().show(); // ...show the previous element (<span> element with the class 'spoiler-btn' in this case) of the selected specific object (previously created <button> element in this case)
+    $(this).remove(); // remove the selected specific object (previously created <button> element in this case) from the page
+});
